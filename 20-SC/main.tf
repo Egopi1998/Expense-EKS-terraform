cac918cbf26616571f09821c6c448fb47d37398c
E.Gopi <egopimani1998@gmail.com>
2025-03-08 20:06:28 +0530

    auto commit

--- 20-SC/main.tf
+++ 20-SC/main.tf
@@ -37,30 +37,6 @@ module "private" {
   common_tags = var.common_tags
   sg_name = "private_${var.project_name}"
   ingress_rules = [
-    { # Bastion to DB
-      from_port = 3306
-      to_port = 3306
-      protocol = "tcp"
-      security_groups = [module.public.sg_id]
-    },
-    { # Bastion to control plane api server
-      from_port = 443
-      to_port = 443
-      protocol = "tcp"
-      security_groups = [module.public.sg_id]
-    },
-    { # ELB to node appliaction on 80 port
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
-      security_groups = [module.public.sg_id]
-    },
-    { # ELB to nodes 
-      from_port = 30000
-      to_port = 32767
-      protocol = "tcp"
-      security_groups = [module.public.sg_id]
-    },
     { # allow all traffic from VPC
       from_port = 0
       to_port = 65535
@@ -77,6 +53,31 @@ resource "aws_security_group_rule" "private-private" {
   protocol                 = "tcp"
   security_group_id        = module.private.sg_id
   source_security_group_id = module.private.sg_id # ✅ Allow traffic within the same SG
+}
+# private sg accepting traffic from public sg
+resource "aws_security_group_rule" "private-private" {
+  type                     = "ingress"
+  from_port                = 30000
+  to_port                  = 32767
+  protocol                 = "tcp"
+  security_group_id        = module.private.sg_id
+  source_security_group_id = module.public.sg_id # from where traffic is coming from
+}
+resource "aws_security_group_rule" "private-private" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = module.private.sg_id
+  source_security_group_id = module.public.sg_id # from where traffic is coming from
+}
+resource "aws_security_group_rule" "private-private" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = module.private.sg_id
+  source_security_group_id = module.public.sg_id # from where traffic is coming from
 }
 
 
