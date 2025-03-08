cd08c6db004dd6c784e653589d61965be8523a45
E.Gopi <egopimani1998@gmail.com>
2025-03-08 18:40:25 +0530

    auto commit

--- 20-SC/local.tf
+++ 20-SC/local.tf
@@ -1,12 +1,5 @@
-module "public" {
-  source = "git::https://github.com/Egopi1998/terraform-aws-SG-module.git?ref=main"
-  project_name = var.project_name
-  environment = var.environment
-  sg_description = "SG for Bastion and public access"
-  vpc_id = data.aws_ssm_parameter.vpc_id.value
-  common_tags = var.common_tags
-  sg_name = "public_${var.project_name}"
-  ingress_rules = [
+locals {
+    public_ingress_rules =  [
     {
       from_port = 22
       to_port = 22
@@ -27,59 +20,49 @@ module "public" {
     }
   ]
 }
-
-module "private" {
-  source = "git::https://github.com/Egopi1998/terraform-aws-SG-module.git?ref=main"
-  project_name = var.project_name
-  environment = var.environment
-  sg_description = "SG for private resources"
-  vpc_id = data.aws_ssm_parameter.vpc_id.value
-  common_tags = var.common_tags
-  sg_name = "private_${var.project_name}"
-  depends_on = [ module.public ]
-  ingress_rules = [
+locals {
+  private_ingress_rules = [
     { # allow all traffic from  private sg to private sg
       from_port = 0
       to_port = 65535
       protocol = "tcp"
       cidr_blocks = []
-      security_groups = [ "self" ]
+      security_groups = "self"
     },
     { # Bastion to DB
       from_port = 3306
       to_port = 3306
       protocol = "tcp"
       cidr_blocks = []
-      security_groups = [ module.public.sg_id ]
+      security_groups = module.public.sg_id
     },
     { # Bastion to control plane api server
       from_port = 443
       to_port = 443
       protocol = "tcp"
       cidr_blocks = []
-      security_groups = [ module.public.sg_id ]
+      security_groups = module.public.sg_id
     },
     { # ELB to node appliaction on 80 port
       from_port = 80
       to_port = 80
       protocol = "tcp"
       cidr_blocks = []
-      security_groups = [ module.public.sg_id ]
+      security_groups = module.public.sg_id
     },
     { # ELB to nodes 
       from_port = 30000
       to_port = 32767
       protocol = "tcp"
       cidr_blocks = []
-      security_groups = [ module.public.sg_id ]
+      security_groups = [module.public.sg_id]
     },
     { # allow all traffic from VPC
       from_port = 0
       to_port = 65535
       protocol = "-1" #accept all protocols
       cidr_blocks = ["10.0.0.0/16"]
+      security_groups = []
     }
   ]
 }
-
-
